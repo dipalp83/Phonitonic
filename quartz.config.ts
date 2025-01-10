bb83d48ae4d2f668f01288e5cc4d13cfa7d9ed30
Dipal Patel <dipalp83@gmail.com>
2025-01-11 02:35:09 +0530

    [comments setup]

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,5 +1,73 @@
-import { QuartzConfig } from "./quartz/cfg"
+import { GlobalConfiguration, QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
+
+var custom_theme : GlobalConfiguration["theme"] = {
+  fontOrigin: "googleFonts",
+  cdnCaching: true,
+  typography: {
+    header: "Schibsted Grotesk",
+    body: "Source Sans Pro",
+    code: "IBM Plex Mono",
+  },
+  colors: {
+    lightMode: {
+      light: "#faf8f8",
+      lightgray: "#e5e5e5",
+      gray: "#b8b8b8",
+      darkgray: "#4e4e4e",
+      dark: "#2b2b2b",
+      secondary: "#284b63",
+      tertiary: "#84a59d",
+      highlight: "rgba(143, 159, 169, 0.15)",
+      textHighlight: "#fff23688",
+    },
+    darkMode: {
+      light: "#161618",
+      lightgray: "#393639",
+      gray: "#646464",
+      darkgray: "#d4d4d4",
+      dark: "#ebebec",
+      secondary: "#7b97aa",
+      tertiary: "#84a59d",
+      highlight: "rgba(143, 159, 169, 0.15)",
+      textHighlight: "#b3aa0288",
+    },
+  },
+}
+
+var orignal_theme : GlobalConfiguration["theme"] = {
+  fontOrigin: "googleFonts",
+  cdnCaching: true,
+  typography: {
+    header: "Schibsted Grotesk",
+    body: "Source Sans Pro",
+    code: "IBM Plex Mono",
+  },
+  colors: {
+    lightMode: {
+      light: "#faf8f8",
+      lightgray: "#e5e5e5",
+      gray: "#b8b8b8",
+      darkgray: "#4e4e4e",
+      dark: "#2b2b2b",
+      secondary: "#284b63",
+      tertiary: "#84a59d",
+      highlight: "rgba(143, 159, 169, 0.15)",
+      textHighlight: "#fff23688",
+    },
+    darkMode: {
+      light: "#161618",
+      lightgray: "#393639",
+      gray: "#646464",
+      darkgray: "#d4d4d4",
+      dark: "#ebebec",
+      secondary: "#7b97aa",
+      tertiary: "#84a59d",
+      highlight: "rgba(143, 159, 169, 0.15)",
+      textHighlight: "#b3aa0288",
+    },
+  },
+} 
 
 /**
  * Quartz 4.0 Configuration
@@ -8,7 +76,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+
+    pageTitle: "🪴 Phonitonics",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -17,48 +86,26 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
-    generateSocialImages: false,
-    theme: {
-      fontOrigin: "googleFonts",
-      cdnCaching: true,
-      typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
-      },
-      colors: {
-        lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-          textHighlight: "#fff23688",
-        },
-        darkMode: {
-          light: "#161618",
-          lightgray: "#393639",
-          gray: "#646464",
-          darkgray: "#d4d4d4",
-          dark: "#ebebec",
-          secondary: "#7b97aa",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-          textHighlight: "#b3aa0288",
-        },
-      },
+    ignorePatterns: ["private", "templates", "-- Templates --",".obsidian", "Excalidraw", "Canvas", "Images"],
+    defaultDateType: "modified",
+    generateSocialImages: {
+
+        colorScheme: "lightMode", // what colors to use for generating image, same as theme colors from config, valid values are "darkMode" and "lightMode"
+        width: 1200, // width to generate with (in pixels)
+        height: 630, // height to generate with (in pixels)
+        excludeRoot: false, // wether to exclude "/" index path to be excluded from auto generated images (false = use auto, true = use default og image)
+
     },
+
+    theme: custom_theme,
+  
+  
   },
   plugins: {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "filesystem"],
+        priority: ["git","frontmatter"],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
@@ -67,7 +114,8 @@ const config: QuartzConfig = {
         },
         keepBackground: false,
       }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: true, enableCheckbox: true }),
+      Plugin.HardLineBreaks(),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,22 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        repo:"dipalp83/Phonitonic",
+        repoId:"R_kgDONooDKw",
+        category:"Announcements",
+        categoryId:"DIC_kwDONooDK84Cl6rI",
+
+        inputPosition: "bottom",
+        reactionsEnabled: true,
+        strict: true,
+        mapping: "url",
+ }
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
@@ -21,6 +36,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
+
   ],
   left: [
     Component.PageTitle(),
@@ -30,10 +46,43 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph({
+      localGraph: {
+        drag: true, // whether to allow panning the view around
+        zoom: true, // whether to allow zooming in and out
+        depth: 1, // how many hops of notes to display
+        scale: 1.1, // default view scale
+        repelForce: 0.5, // how much nodes should repel each other
+        centerForce: 0.3, // how much force to use when trying to center the nodes
+        linkDistance: 30, // how long should the links be by default?
+        fontSize: 0.6, // what size should the node labels be?
+        opacityScale: 1, // how quickly do we fade out the labels when zooming out?
+        removeTags: [], // what tags to remove from the graph
+        showTags: true, // whether to show tags in the graph
+        focusOnHover: true, // whether to highlight nodes when hovering over them
+      },
+      globalGraph: {
+        drag: true,
+        zoom: true,
+        depth: -1,
+        scale: 0.9,
+        repelForce: 0.5,
+        centerForce: 0.3,
+        linkDistance: 30,
+        fontSize: 0.6,
+        opacityScale: 1,
+
+        focusOnHover: true,
+
+        removeTags: ['excalidraw'], // what tags to remove from the graph
+        showTags: true, // whether to show tags in the graph
+      },
+    }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
+
+  
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
